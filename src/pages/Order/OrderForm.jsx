d4bb15a31d0aa1af71663ac611e3bb365ec9db84
Jeet-12 <dewanganjd2003@gmail.com>
2025-08-21 00:45:35 +0530

    order

--- src/pages/Order/OrderForm.jsx
+++ src/pages/Order/OrderForm.jsx
@@ -108,7 +108,7 @@ const OrderForm = () => {
   };
 
   return (
-    <div className="flex flex-col justify-center bg-[#e6f0df] min-h-screen py-4 px-2 sm:py-8">
+    <div className="flex flex-col justify-center bg-[#e6f0df] min-h-screen py-4 px-4 sm:py-8">
       <div className="mx-auto w-full max-w-4xl">
         <div className="bg-white border border-gray-200 shadow-lg w-full rounded-lg p-3 sm:p-4 md:p-6">
           <SectionTitle title={`Order Form`} path={`Home > Order Form`} />
